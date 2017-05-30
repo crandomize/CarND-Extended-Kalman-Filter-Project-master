db42d567b16fd1c7c05522cfbe502b4130471f2c
carlos <cafernandez@gmail.com>
2017-05-30 11:49:51 +0200

    Implemented changes
    Implemented final changes

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,6 +3,9 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+#include <iostream>
+using namespace std;
+
 KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
@@ -18,22 +21,62 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si; 
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+
+  // To calculate z_pred we need to convert from cartesian to polar.
+  double rho_pred, phi_pred, rhodot_pred;
+  rho_pred = sqrt(pow(x_[0], 2) + pow(x_[1], 2));
+
+  phi_pred = 0.0;
+  if (fabs(x_[0]) > 0.001) {
+    phi_pred  = atan2(x_[1], x_[0]);
+  }
+
+  rhodot_pred = 0.0;
+  if (fabs(rho_pred) > 0.001) {
+    rhodot_pred = (x_[0] * x_[2] + x_[1] * x_[3]) / rho_pred;
+  }
+  VectorXd z_pred(3);
+  z_pred << rho_pred, phi_pred, rhodot_pred;
+
+	//VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+
+  //if radar measurement (size is 3), normalize angle
+  if (y.size() == 3) y(1) = atan2(sin(y(1)), cos(y(1))); 
+
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+
 }
